f4f1193a6dbdc347ccfd4ef41b348dd786ed01f3
Kofi Gumbs <h.kofigumbs@gmail.com>
2016-10-07 13:46:29 -0600

    Modernize and minimalize

--- popup.js
+++ popup.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    
-  var textarea = document.createElement("textarea");
-  textarea.style.position = "absolute"
-  textarea.style.left = "-100%"
-  
-  textarea.value = "r\xE9sum\xE9";
-  document.body.appendChild(textarea);
-  textarea.select();
-  document.execCommand("Copy");
-  document.body.removeChild(textarea);
-
+    document.getElementsByTagName("textarea")[0].select();
+    document.execCommand("Copy");
 });
